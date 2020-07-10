b8597a3969f1702d1b69a75414a86ab91b5c5f8b
Nicolas de Fontaine <nicolas.defontaine@apizee.com>
2020-07-10 18:15:39 +0200

    Config Apizee

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'apizee' );
 define( 'DB_PASSWORD', 'Lyliana01' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '51.38.38.193' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
